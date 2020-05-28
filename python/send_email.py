6e8b2a6ba46e69dc88c75a9729a17c381349848b
NBOCCHINI <nicholasbocchini@gmail.com>
2020-05-27 20:28:00 -0500

    Updated the send_email python program

--- python/send_email.py
+++ python/send_email.py
@@ -1,4 +1,9 @@
-import email, smtplib, ssl
+import os
+import email
+import smtplib
+import ssl
+import gzip
+import shutil
 
 from email import encoders
 from email.mime.base import MIMEBase
@@ -7,12 +12,14 @@ from email.mime.text import MIMEText
 
 port = 587
 smtp_server = "smtp.gmail.com"
-password = os.environment["EMAIL_PASS"]
-sender = os.environment["EMAIL_ACCOUNT"]
+password = os.environ["EMAIL_PASS"]
+sender = os.environ["EMAIL_ACCOUNT"]
 receiver = "gardenviewupdates@gmail.com"
+video_root = "/opt/gardenview/output"
+archive_root = "/opt/gardenview/archive"
 
-subject = "Sending attached video"
-body = "See the attached mp4 file for video from the raspberrypi."
+subject = "GardenView Updates!"
+body = "See the attached files for video and pictures to see the motion detected by GardenView."
 
 message = MIMEMultipart()
 message["From"] = sender
@@ -20,19 +27,26 @@ message["To"] = receiver
 message["Subject"] = subject
 message.attach(MIMEText(body, "plain"))
 
-filename = "/Users/NicholasBocchini/Development/gardenview/01-20200527165618.mov"
+dir_list = [os.path.join(video_root, f) for f in os.listdir(video_root)]
+file_list = [f for f in dir_list if os.path.isfile(f)]
 
-with open(filename, "rb") as attachment:
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(attachment.read())
+for file in file_list:
+    with open(file, "rb") as attachment:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
 
-encoders.encode_base64(part)
-part.add_header(
-    "Content-Disposition",
-    f"attachment; filename={filename}",
-)
+    encoders.encode_base64(part)
+    part.add_header(
+        "Content-Disposition",
+        "attachment",
+        filename=file
+    )
+    message.attach(part)
 
-message.attach(part)
+    with open(file, "rb") as f_in, gzip.open(os.path.join(archive_root, os.path.basename(file) + ".gz"), "wb") as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+    os.remove(file)
 
 context = ssl.create_default_context()
 with smtplib.SMTP(smtp_server, port) as server:
